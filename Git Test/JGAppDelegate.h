a355c5079c9fc350111129d3bb3c1e1f0e8334c8
Justin Gazlay <justingazlay@Justin-Gazlays-iMac.local>
2014-06-02 21:50:14 -0600

    Initial Commit

---
+++ Git Test/JGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JGAppDelegate.h
+//  Git Test
+//
+//  Created by Justin Gazlay on 6/2/14.
+//  Copyright (c) 2014 Justin Gazlay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/JGViewController.h
@@ -0,0 +1,13 @@
+//
+//  JGViewController.h
+//  Git Test
+//
+//  Created by Justin Gazlay on 6/2/14.
+//  Copyright (c) 2014 Justin Gazlay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGViewController : UIViewController
+
+@end
