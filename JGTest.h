2595dd66264a0ec468b08b8910c4177442a9a3a1
Justin Gazlay <justingazlay@Justin-Gazlays-iMac.local>
2014-06-02 22:05:44 -0600

    added a test class

---
+++ JGTest.h
@@ -0,0 +1,13 @@
+//
+//  JGTest.h
+//  Git Test
+//
+//  Created by Justin Gazlay on 6/2/14.
+//  Copyright (c) 2014 Justin Gazlay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JGTest : NSObject
+
+@end
---
+++ JGTest.m
@@ -0,0 +1,13 @@
+//
+//  JGTest.m
+//  Git Test
+//
+//  Created by Justin Gazlay on 6/2/14.
+//  Copyright (c) 2014 Justin Gazlay. All rights reserved.
+//
+
+#import "JGTest.h"
+
+@implementation JGTest
+
+@end
